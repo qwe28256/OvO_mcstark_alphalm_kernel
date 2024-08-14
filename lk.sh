6bc3813affb6e37129d59b9be5a97790e0f688cd
Qwe_OvO_Dev_Team <qwe28256.swu@vip.163.com>
2024-08-14 14:05:11 +0800

    Update lk.sh

--- lk.sh
+++ lk.sh
@@ -1,6 +1,5 @@
-mv ../../toolchain/clang-17/bin/llvm-objcopy ../../toolchain/clang-17/bin/llvm-objcopy.bak
-cp -rf /usr/bin/aarch64-linux-gnu-objcopy ../../toolchain/clang-17/bin/llvm-objcopy 
-
-make O=out $args -j20 
-cp -rf ../../toolchain/clang-17/bin/llvm-objcopy.bak ../../toolchain/clang-17/bin/llvm-objcopy 
+mv ../../clang/bin/llvm-objcopy ../../clang/bin/llvm-objcopy.bak
+cp -rf /usr/bin/aarch64-linux-gnu-objcopy ../../clang/bin/llvm-objcopy 
+make $args -j20 
+cp -rf ../../clang/bin/llvm-objcopy.bak ../../clang/bin/llvm-objcopy 
 
