ec3fc8f8daea36de7d12e03b057c2264ab5fba71
Qwe_OvO_Dev_Team <qwe28256.swu@vip.163.com>
2024-08-14 14:05:39 +0800

    Update lk.sh

--- lk.sh
+++ lk.sh
@@ -2,4 +2,4 @@ mv ../../clang/bin/llvm-objcopy ../../clang/bin/llvm-objcopy.bak
 cp -rf /usr/bin/aarch64-linux-gnu-objcopy ../../clang/bin/llvm-objcopy 
 make $args -j20 
 cp -rf ../../clang/bin/llvm-objcopy.bak ../../clang/bin/llvm-objcopy 
-
+echo "Build done"
